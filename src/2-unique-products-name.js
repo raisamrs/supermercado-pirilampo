2943ee9b8e3d6dbc7681ff1b3eb1afbb6a666f43
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-10-28 18:44:42 -0300

    Requisito 2 finalizado

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const getUniqueProductsName = () => {
-  // Desenvolva seu código dentro dessa função...
+const getUniqueProductsName = (mainArray) => {
+  mainArray = stockProducts;
+
+  let arrayProductsName;
+  let formater = /\s*,\s*/;
+  if (mainArray.length > 0) {
+    let counter = mainArray[0].productName;
+    for (let index = 1; index < mainArray.length; index += 1) {
+      counter = `${counter}, ${mainArray[index].productName}`;
+    }
+    arrayProductsName = counter.split(formater);
+  } else if (mainArray.length === 0) {
+    arrayProductsName = [];
+  }
+  return arrayProductsName;
 };
 
+getUniqueProductsName('./data.json');
 module.exports = { getUniqueProductsName };
