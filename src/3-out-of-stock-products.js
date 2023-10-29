bb359d9b4835e63a36115b27f0f7a1248ad0f22c
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-10-28 22:15:04 -0300

    Requisito 3 finalizado

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
-const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+const getOutOfStockProducts = (mainArray) => {
+  mainArray = stockProducts;
+  /* let arrayProductsOutOfStock = []; */
+  let formater = /\s*,\s*/;
+  let counter = '';
+  let index = 0;
+  for (index = 0; index < mainArray.length; index += 1) {
+    let productOutOfStock = mainArray[index].productName;
+    if (mainArray[index].quantityInStock === 0) {
+      counter = `${counter}, ${productOutOfStock}`;
+    }
+  } counter = counter.slice(2).split(formater);
+  return counter;
 };
 
+getOutOfStockProducts(stockProducts);
 module.exports = { getOutOfStockProducts };
