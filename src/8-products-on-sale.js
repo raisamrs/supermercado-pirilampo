7e9d76462b48c9af67f1a6b67101ab8efac70478
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-10-29 14:33:07 -0300

    Requisito 8 finalizado

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+const getProductsOnSale = (mainArray) => {
+  mainArray = stockProducts;
+  let objectExpected = [];
+  for (let i = 0; i < mainArray.length; i += 1) {
+    if (mainArray[i].onSale === true) {
+      objectExpected.push({
+        description: mainArray[i].description,
+        formattedPrice: `R$ ${mainArray[i].price}`,
+        onSale: true,
+      });
+    }
+  }
+  return objectExpected;
 };
 
 module.exports = { getProductsOnSale };
