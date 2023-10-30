3ba6aebc5dc5f45cb5b99f0ae2f6047b26f60f79
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-10-29 21:48:50 -0300

    Requisito 9  finalizado

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
-const getProductsWithAllergyOrIntoleranceInfo = () => {
-
+const getProductsWithAllergyOrIntoleranceInfo = (mainArray) => {
+  mainArray = stockProducts;
+  let objectExpected = [];
+  for (let i = 0; i < mainArray.length; i += 1) {
+    if (mainArray[i].allergyOrIntolerance !== undefined) {
+      let productAllerOrIntoNoComma = mainArray[i].allergyOrIntolerance.join(' ');
+      objectExpected.push({
+        description: mainArray[i].description,
+        formattedPrice: `R$ ${mainArray[i].price}`,
+        allergyOrIntoleranceMessage: `Pode conter: ${productAllerOrIntoNoComma}`,
+      });
+    }
+  }
+  return objectExpected;
 };
-
+getProductsWithAllergyOrIntoleranceInfo(stockProducts);
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
